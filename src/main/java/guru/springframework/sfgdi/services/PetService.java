8abbc02b1032b8998a4873134137d504aaaca716
yassinerzk <yassinerazzouki@gmail.com>
2020-02-07 00:24:40 +0100

    creating different DI.

---
+++ src/main/java/guru/springframework/sfgdi/services/PetService.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgdi.services;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by jt on 12/28/19.
+ */
+
+public interface PetService {
+
+    String getPetType();
+}
